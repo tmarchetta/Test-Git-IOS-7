54d0cd009b8b2747b88de1a1a263fabdab34731b
tmarchetta <theresa.marchetta@gmail.com>
2013-10-22 12:47:01 -0400

    added a test class

---
+++ GitHub Test/TMSTester.h
@@ -0,0 +1,13 @@
+//
+//  TMSTester.h
+//  GitHub Test
+//
+//  Created by Theresa Marchetta on 10/22/13.
+//  Copyright (c) 2013 Theresa Marchetta Studio. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TMSTester : NSObject
+
+@end
---
+++ GitHub Test/TMSTester.m
@@ -0,0 +1,13 @@
+//
+//  TMSTester.m
+//  GitHub Test
+//
+//  Created by Theresa Marchetta on 10/22/13.
+//  Copyright (c) 2013 Theresa Marchetta Studio. All rights reserved.
+//
+
+#import "TMSTester.h"
+
+@implementation TMSTester
+
+@end
